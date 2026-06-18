0fcd2d3665a742dc07faaeda67dcbdce9125881b
lucavaresio <luca.varesio@edu-its.it>
2026-06-18 20:18:04 +0200

    Fixato il vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite';
-
 export default defineConfig({
   server: {
     proxy: {
@@ -12,5 +10,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api-proxy/, '')
       }
     }
-  }
+  },
+  plugins: [react()],
+  base: './', 
 });
